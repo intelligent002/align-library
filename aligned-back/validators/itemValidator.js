f704e9e174dcdbc8fea35c7e8de61ef21b88c127
Arkady Rosenberg <intelligent002@gmail.com>
2025-04-07 12:43:34 +0300

    fix youtube issue

--- aligned-back/validators/itemValidator.js
+++ aligned-back/validators/itemValidator.js
@@ -1,23 +1,18 @@
 exports.validateItemInput = (name, url) => {
   const errors = {};
-  let type = null;
 
   if (!name || name.trim() === '') {
     errors.name = 'Name is required';
   }
 
-  const urlPattern = /^https?:\/\/.+/;
+  // Basic URL pattern: starts with http:// or https://
+  const urlPattern = /^https?:\/\/[\w.-]+\.[a-z]{2,}(\/\S*)?$/i;
   if (!url || !urlPattern.test(url)) {
     errors.url = 'Valid URL is required';
-  } else if (url.includes('youtube.com') || url.includes('youtu.be')) {
-    type = 'youtube';
-  } else {
-    type = 'link';
   }
 
   return {
     isValid: Object.keys(errors).length === 0,
     errors,
-    type
   };
 };
